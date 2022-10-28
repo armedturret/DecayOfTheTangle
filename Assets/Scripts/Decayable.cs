22041df8b4900a35338e53ddf6a5c0989e35429c
Robert White <rjwhitenh@outlook.com>
2022-10-28 18:32:18 -0400

    Added the ability for tiles to decay

---
+++ Assets/Scripts/Decayable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Decayable : MonoBehaviour
+{
+    public LayerMask decayableLayer;
+    public float decayRate = 1f;
+    public float decayThreshold = 0.2f;
+    public float decaySpreadRadius = 1f;
+
+    protected float _decay = 0f;
+    public bool _decaying = false;
+
+    public void StartDecay()
+    {
+        _decaying = true;
+    }
+
+    protected virtual void Update()
+    {
+        //start decay tick
+        if (_decaying)
+        {
+            _decay += Time.deltaTime * decayRate;
+            _decay = Mathf.Clamp(_decay, 0f, 1f);
+        }
+
+        //check if past threshold (perform OnDecayed and check for infection)
+        if(_decay >= decayThreshold)
+        {
+            OnDecayed();
+
+            //decay spread check
+            Collider2D[] detected = Physics2D.OverlapCircleAll(transform.position, decaySpreadRadius, decayableLayer);
+            foreach(var other in detected)
+            {
+                other.gameObject.GetComponent<Decayable>().StartDecay();
+            }
+        }
+    }
+
+    protected virtual void OnDecayed()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DecayableTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class DecayableTile : Decayable
+{
+    private Rigidbody2D _rb2d;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnDecayed()
+    {
+        //make this object dynamic
+        _rb2d.bodyType = RigidbodyType2D.Dynamic;
+
+        //calculate the color interpolating with decay
+        _spriteRenderer.color = new Color(1f - _decay, 1f - _decay, 1f - _decay);
+
+        //delete if completely decayed
+        if (_decay >= 1f)
+            Destroy(gameObject);
+    }
+}
