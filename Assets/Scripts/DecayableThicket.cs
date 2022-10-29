d9c24bbfc3ab89def91fb9bb97b64c2998311503
Robert White <rjwhitenh@outlook.com>
2022-10-29 09:50:19 -0400

    Added decaying hazards

---
+++ Assets/Scripts/DecayableThicket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Hazard))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class DecayableThicket : Decayable
+{
+    public float hazardThreshold = 0.75f;
+
+    private Rigidbody2D _rb2d;
+    private SpriteRenderer _spriteRenderer;
+    private Hazard _hazard;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rb2d = GetComponent<Rigidbody2D>();
+        _hazard = GetComponent<Hazard>();
+        _hazard.enabled = false;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        //calculate the color interpolating with decay
+        _spriteRenderer.color = new Color(1f, (1f - _decay), (1f - _decay));
+
+        //enable phsyics and destroy connections
+        if(_decay >= hazardThreshold)
+        {
+            //enable the hazard
+            _hazard.enabled = true;
+
+            //break connections
+            if(GetComponent<FixedJoint2D>())
+                GetComponent<FixedJoint2D>().enabled = false;
+        }
+    }
+}
--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -7,12 +7,27 @@ public class Hazard : MonoBehaviour
     //player layers
     public LayerMask playerLayer;
 
+    private PlayerController _player;
+
+    private void Update()
+    {
+        if (_player != null)
+            _player.Kill();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(Utils.InLayerMask(playerLayer, collision.gameObject.layer))
         {
-            //touched player (MURDER)
-            collision.gameObject.GetComponent<PlayerController>().Kill();
+            _player = collision.gameObject.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (Utils.InLayerMask(playerLayer, collision.gameObject.layer))
+        {
+            _player = null;
         }
     }
 }
