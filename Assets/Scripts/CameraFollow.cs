05992f28fd429799fd74576be49e112ef0072f27
Robert White <rjwhitenh@outlook.com>
2022-10-29 13:02:19 -0400

    Added a camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //attach this script to the player to get cam to follow it
+    public enum FOLLOW_TYPE { Static, Horizonal, Vertical, Both};
+
+    public FOLLOW_TYPE followType = FOLLOW_TYPE.Static;
+
+
+    private void Update()
+    {
+        Vector3 targetPos = Camera.main.transform.position;
+        switch (followType)
+        {
+            case FOLLOW_TYPE.Static:
+                return;
+            case FOLLOW_TYPE.Horizonal:
+                targetPos.x = transform.position.x;
+                break;
+            case FOLLOW_TYPE.Vertical:
+                targetPos.y = transform.position.y;
+                break;
+            case FOLLOW_TYPE.Both:
+                targetPos.x = transform.position.x;
+                targetPos.y = transform.position.y;
+                break;
+        }
+
+        Camera.main.transform.position = targetPos;
+    }
+}
