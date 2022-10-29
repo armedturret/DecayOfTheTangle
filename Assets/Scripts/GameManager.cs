daffb6c149e387f6bbdb2ffd04f7e0426a4a3787
Robert White <rjwhitenh@outlook.com>
2022-10-29 15:19:35 -0400

    Added a basic main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int levelOneIndex = 1;
+    public int finalLevelIndex = 2;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetLevel()
+    {
+        return PlayerPrefs.GetInt("level", levelOneIndex);
+    }
+
+    //sets highest unlock value
+    public void UnlockLevel(int buildIndex)
+    {
+        int newUnlock = Mathf.Max(GetLevel(), buildIndex);
+        newUnlock = Mathf.Min(newUnlock, finalLevelIndex);
+        PlayerPrefs.SetInt("level", newUnlock);
+        PlayerPrefs.Save();
+    }
+
+    public void ResetSave()
+    {
+        PlayerPrefs.SetInt("level", levelOneIndex);
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public AudioMixer masterMixer;
+
+    public void NewGame()
+    {
+        GameManager.instance.ResetSave();
+        Continue();
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(GameManager.instance.GetLevel());
+    }
+
+    public void SetSFXVol(float percent)
+    {
+        percent = Mathf.Max(percent, 0.001f);
+        masterMixer.SetFloat("SFXVol", Mathf.Log10(percent) * 20);
+    }
+
+    public void SetMusicVol(float percent)
+    {
+        percent = Mathf.Max(percent, 0.001f);
+        masterMixer.SetFloat("MusicVol", Mathf.Log10(percent) * 20);
+    }
+}
